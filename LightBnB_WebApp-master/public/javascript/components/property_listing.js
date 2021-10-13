14a26267f9b39658ce452a4bef0bda562cc123ad
rewpt <zpermack@gmail.com>
2021-10-13 21:12:25 +0000

    add template for property listing

--- LightBnB_WebApp-master/public/javascript/components/property_listing.js
+++ LightBnB_WebApp-master/public/javascript/components/property_listing.js
@@ -15,11 +15,18 @@ $(() => {
             <li>parking_spaces: ${property.parking_spaces}</li>
           </ul>
           ${isReservation ? 
-            `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
-            : ``}
+            `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` : `<button id="reserve-property-${property.id}" class="reserve-button">Reserve</button>`}
           <footer class="property-listing__footer">
             <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
             <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            ${isReservation.upcoming ? 
+              `<button id="update-property-${property.id} class="update-button">Update</button>
+                <button id="delete-property-${property.id} class="delete-button">Delete</button>
+              ` : ``
+            }
+            <span id="review-details-${property.id}" class="review_details">
+              Browse ${property.review_count} reviews
+            </span>
           </footer>
         </section>
       </article>
