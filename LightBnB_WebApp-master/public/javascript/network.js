b117366d5b46eaa831157c02f1924ea70bda0608
rewpt <zpermack@gmail.com>
2021-10-13 21:08:21 +0000

    add ajax requests for update, delete upcoming reservations

--- LightBnB_WebApp-master/public/javascript/network.js
+++ LightBnB_WebApp-master/public/javascript/network.js
@@ -52,3 +52,40 @@ const submitProperty = function(data) {
     data,
   });
 }
+
+const submitReservation = function(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/reservations",
+    data,
+  })
+}
+
+function getUpcomingReservations() {
+  let url = "/api/reservations/upcoming";
+  return $.ajax({
+    url,
+  });
+}
+
+function getIndividualReservation(reservationId) {
+  let url = `/api/reservations/${reservationId}`
+  return $.ajax({
+    url,
+  })
+}
+
+const updateReservation = function(data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/reservations/${data.reservation_id}`,
+    data,
+  })
+}
+
+const deleteReservation = function(data) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/reservations/${data.reservation_id}`
+  })
+}
